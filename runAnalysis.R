9339ed1ae172b325c0940fa733246d5cdc0855a1
dingdata <dingbro@gmail.com>
2014-10-22 00:24:01 +0800

    Update runAnalysis.R

--- runAnalysis.R
+++ runAnalysis.R
@@ -63,5 +63,5 @@ allData$subject <- as.factor(allData$subject)
 
 tidy = aggregate(allData, by=list(activity = allData$activity, subject=allData$subject), mean)
 
-write.table(tidy, "tidy.txt", sep="\t")
+write.table(tidy, "tidy.txt", sep="\t", row.name = FALSE)
 
